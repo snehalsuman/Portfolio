c3e86d13f5d80e743aff40150a71d30aaec4aae9
Snehal Suman <snehals0203@gmail.com>
2025-04-09 14:29:20 +0530

    added more functionalities

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -4,7 +4,6 @@ import ezzgrow from "../../assets/ezzgrow.png";
 import chatty from "../../assets/chatapp.png";
 import weather from "../../assets/weather.png";
 
-
 const Projects = () => {
   return (
     <div id="Projects" className="p-10 md:p-24 text-white ">
@@ -14,16 +13,22 @@ const Projects = () => {
           title="Ezzgrow"
           main="EzzGrow is a Next.js based platform offering technical services and business consultancy with modern UI/UX."
           image={ezzgrow}
+          demoLink="https://ezzgrow.vercel.app/"
+          codeLink="https://github.com/snehalsuman/ezzgrow-main/tree/main/ezzgrow-final-main"
         />
         <ProjectCard
-          title="chatty"
+          title="Chatty"
           main="A real-time chat application built with React, Node.js, Socket.io, MongoDB and supports live chat and private/group messaging."
           image={chatty}
+          demoLink=""
+          codeLink="https://github.com/snehalsuman/chatapp-backend/tree/main/src"
         />
         <ProjectCard
           title="Weather App"
           main="A responsive weather forecast app built using React, Tailwind CSS and provides real-time weather updates using API calls."
           image={weather}
+          demoLink="https://weather-app-99.vercel.app/"
+          codeLink="https://github.com/snehalsuman/Weather-App"
         />
       </div>
     </div>
